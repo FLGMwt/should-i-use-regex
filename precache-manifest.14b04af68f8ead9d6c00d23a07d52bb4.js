dafc9c95c6aff2114d4c49cf26d737c88e9026d5
Ryan Stelly <ryan@skyrisepro.com>
2020-06-06 13:01:02 -0500

    Updates

---
+++ precache-manifest.14b04af68f8ead9d6c00d23a07d52bb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd21a0a764a555d3a5f4970d80c4eed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d24474b629f3f9ad704",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "afac68fc9b4a6d6283c9",
+    "url": "/static/js/2.7f8635d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7f8635d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d24474b629f3f9ad704",
+    "url": "/static/js/main.4f4c2d17.chunk.js"
+  },
+  {
+    "revision": "5677dfe762a8a8d61e17",
+    "url": "/static/js/runtime-main.4c482a43.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/should-i-use-regex/precache-manifest.c80b5a3081438a91e836f87b083e17a2.js"
+  "/precache-manifest.14b04af68f8ead9d6c00d23a07d52bb4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/should-i-use-regex/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
