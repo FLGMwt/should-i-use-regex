d17a0f71041e0580e0b2794ad85622452b196a07
Ryan Stelly <ryan@skyrisepro.com>
2020-06-06 12:10:38 -0500

    initial site

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
 
-function App() {
+const textStyle = css({ fontSize: 24 });
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div
+      css={{
+        backgroundColor: '#A7CCED',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <h1 css={{ margin: 0, fontSize: 58 }}>
+        Should I use regex?{' '}
+        <span role="img" aria-label="Thinking face">
+          🤔
+        </span>
+      </h1>
+      <section css={{ marginTop: 16, marginBottom: 16 }}>
+        <p css={textStyle}>
+          To determine if a regular expression is right for your use case, ask
+          yourself the following:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ol>
+          <li css={textStyle}>
+            Will using a regex here help dismantle white supremacy?
+          </li>
+        </ol>
+        <p css={textStyle}>
+          If the answer to any of these questions is yes, go for it!
+        </p>
+      </section>
+      <section>
+        <ul css={{ listStyle: 'none', textAlign: 'center' }}>
+          <li>
+            <a
+              href="https://support.eji.org/give/153413/#!/donation/checkout"
+              css={textStyle}
+            >
+              Support the Equal Justice Initiative
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.versobooks.com/lists/4732-abolition-and-black-struggle"
+              css={textStyle}
+            >
+              #AbolishThePolice
+            </a>
+          </li>
+          <li>
+            <a href="https://unitedwedream.org/abolishice/" css={textStyle}>
+              #AbolishIce
+            </a>
+          </li>
+        </ul>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
