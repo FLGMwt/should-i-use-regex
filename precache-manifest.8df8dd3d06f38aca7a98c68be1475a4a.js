5e9c5807858b8e944cbb86b8285afde0c773c5d0
Ryan Stelly <ryan@skyrisepro.com>
2020-06-06 12:14:37 -0500

    Updates

---
+++ precache-manifest.8df8dd3d06f38aca7a98c68be1475a4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c0ead4c5dfc44b9461293582b308684",
+    "url": "/FLGMwt/should-i-use-regex/index.html"
+  },
+  {
+    "revision": "eb886e98679c446b1981",
+    "url": "/FLGMwt/should-i-use-regex/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "afac68fc9b4a6d6283c9",
+    "url": "/FLGMwt/should-i-use-regex/static/js/2.7f8635d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FLGMwt/should-i-use-regex/static/js/2.7f8635d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb886e98679c446b1981",
+    "url": "/FLGMwt/should-i-use-regex/static/js/main.574a1ade.chunk.js"
+  },
+  {
+    "revision": "b72fcab4df9354b5c3ee",
+    "url": "/FLGMwt/should-i-use-regex/static/js/runtime-main.e213f540.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/FLGMwt/should-i-use-regex/precache-manifest.8df8dd3d06f38aca7a98c68be1475a4a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FLGMwt/should-i-use-regex/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
